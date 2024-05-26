6e65941f0c5180ebaf44d6011f1d679c16fd6fd7
Prashant Erappa Yelurkar <prashantyelurkar2004@gmail.com>
2024-05-26 14:28:22 +0530

    first commit

---
+++ src/Components/Layout/page.tsx
@@ -0,0 +1,18 @@
+import Head from "next/head";
+import { ReactNode } from "react";
+
+interface LayoutProps {
+  children: ReactNode;
+  title: string;
+}
+
+export default function Layout({ children, title }: LayoutProps) {
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+      </Head>
+      <div>{children}</div>
+    </>
+  );
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import Layout from "@/Components/Layout/page";
+
+export default function Home() {
+  return (
+    <Layout title="Main">
+      <h1>Hello</h1>
+      <p>dfsd</p>
+    </Layout>
+  );
+}

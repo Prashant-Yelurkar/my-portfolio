aca274db9079309c841def399df227f737a5ff45
Prashant Erappa Yelurkar <prashantyelurkar2004@gmail.com>
2024-05-30 18:49:40 +0530

    Done

--- src/Components/Sections/Skill.tsx
+++ src/Components/Sections/Skill.tsx
@@ -15,10 +15,6 @@ export default function Skills() {
           desc=" Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas."
         />
         <h1>Languages</h1>
-        <h1>Languages</h1>
-        <h1>Languages</h1>
-        <h1>Languages</h1>
-        <h1>Languages</h1>
 
         <Grid minmax={300} gap={30}>
           {ProgrammingLanguages.map((value, index) => {
